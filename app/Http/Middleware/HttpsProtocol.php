8e8e66f7386a921b8a389aa084083bf05518af4f
bungnewbie <lamaaurizkhal@gmail.com>
2018-04-19 20:12:37 -1100

    New file protocol

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+use Closure;
+
+/**
+* HttpsProtocol
+*/
+class HttpsProtocol
+{
+	public function handle($request, Closure $next)
+	{
+		if (!$request->secure() && env('APP_ENV') === 'prod' ) {
+			return redirect()->secure($request->getRequestUri());
+		}
+		return $next($request);
+	}
+}
