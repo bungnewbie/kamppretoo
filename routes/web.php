8ba0b48b0c8871f8c35c283a26ac6ab8bfaa6d3b
bungnewbie <lamaaurizkhal@gmail.com>
2018-04-19 19:57:10 -1100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', ['http' => 'force.ssl'], function(){
+Route::get('https://kampretoo.herokuapp.com/', ['http' => 'force.ssl'], function(){
     return view('welcome');
 });
 
