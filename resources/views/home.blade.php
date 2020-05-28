35e861825096edfbecab80f763fd46aa611aa51d
Rizkhal Lamaau <lamaaurizkhal@gmail.com>
2020-05-28 10:12:30 +0800

    --remove facebook and twitter share

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -89,14 +89,14 @@
                   class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center"
                 >
                   <div class="py-6 px-3 mt-32 sm:mt-0">
-                    <button
+                    {{-- <button
                       type="button"
                       class="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
                     ><i class="fab fa-twitter"></i></button>
                     <button
                       type="button"
                       class="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
-                    ><i class="fab fa-facebook"></i></button>
+                    ><i class="fab fa-facebook"></i></button> --}}
                     <a
                       target="blank"
                       href="whatsapp://send?text=Bagikan balasan tentang {{logged_in_user()->name}} tanpa dikenali  {{user_url()}}"
